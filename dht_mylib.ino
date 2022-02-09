af1be46057795d410718d84a05cf4597517d4b55
nasatame <takanomoziretu@yahoo.co.jp>
2022-02-09 20:27:13 +0900

    Add files via upload

---
+++ dht_mylib.ino
@@ -0,0 +1,260 @@
+#include "Arduino.h"
+
+#define MIN_INTERVAL 2000 /**< min interval value */
+
+#define DEBUG_PRINTER                                                          \
+  Serial /**< Define where debug output will be printed.                       \
+          */
+
+/* Setup debug printing macros. */
+#ifdef DHT_DEBUG
+#define DEBUG_PRINT(...)                                                       \
+  { DEBUG_PRINTER.print(__VA_ARGS__); }
+#define DEBUG_PRINTLN(...)                                                     \
+  { DEBUG_PRINTER.println(__VA_ARGS__); }
+#else
+#define DEBUG_PRINT(...)                                                       \
+  {} /**< Debug Print Placeholder if Debug is disabled */
+#define DEBUG_PRINTLN(...)                                                     \
+  {} /**< Debug Print Line Placeholder if Debug is disabled */
+#endif
+
+#define TIMEOUT                                                                \
+  UINT32_MAX /**< Used programmatically for timeout.                           \
+                   Not a timeout duration. Type: uint32_t. */
+
+uint8_t _pin = 2;
+uint8_t data[5];
+
+uint32_t _lastreadtime, _maxcycles;
+bool _lastresult;
+uint8_t pullTime; // Time (in usec) to pull up data line before reading
+
+
+uint8_t _bit, _port;
+
+uint32_t expectPulse(bool level) {
+#if (F_CPU > 16000000L)
+  uint32_t count = 0;
+#else
+  uint16_t count = 0; // To work fast enough on slower AVR boards
+#endif
+// On AVR platforms use direct GPIO port access as it's much faster and better
+// for catching pulses that are 10's of microseconds in length:
+#ifdef __AVR
+  uint8_t portState = level ? _bit : 0;
+  while ((*portInputRegister(_port) & _bit) == portState) {
+    if (count++ >= _maxcycles) {
+      return TIMEOUT; // Exceeded timeout, fail.
+    }
+  }
+// Otherwise fall back to using digitalRead (this seems to be necessary on
+// ESP8266 right now, perhaps bugs in direct port access functions?).
+#else
+  while (digitalRead(_pin) == level) {
+    if (count++ >= _maxcycles) {
+      return TIMEOUT; // Exceeded timeout, fail.
+    }
+  }
+#endif
+
+  return count;
+}
+
+bool read(bool force) {
+  // Check if sensor was read less than two seconds ago and return early
+  // to use last reading.
+  uint32_t currenttime = millis();
+  if (!force && ((currenttime - _lastreadtime) < MIN_INTERVAL)) {
+    return _lastresult; // return last correct measurement
+  }
+  _lastreadtime = currenttime;
+
+  // Reset 40 bits of received data to zero.
+  data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+
+  // Send start signal.  See DHT datasheet for full signal diagram:
+  //   http://www.adafruit.com/datasheets/Digital%20humidity%20and%20temperature%20sensor%20AM2302.pdf
+
+  // Go into high impedence state to let pull-up raise data line level and
+  // start the reading process.
+  pinMode(_pin, INPUT_PULLUP);
+  delay(1);
+
+  // First set data line low for a period according to sensor type
+  pinMode(_pin, OUTPUT);
+  digitalWrite(_pin, LOW);
+  delay(20); // data sheet says at least 18ms, 20ms just to be safe
+
+  uint32_t cycles[80];
+  {
+    // End the start signal by setting data line high for 40 microseconds.
+    pinMode(_pin, INPUT_PULLUP);
+
+    // Delay a moment to let sensor pull data line low.
+    delayMicroseconds(pullTime);
+
+    // Now start reading the data line to get the value from the DHT sensor.
+
+    // Turn off interrupts temporarily because the next sections
+    // are timing critical and we don't want any interruptions.
+    noInterrupts();
+
+    // First expect a low signal for ~80 microseconds followed by a high signal
+    // for ~80 microseconds again.
+    if (expectPulse(LOW) == TIMEOUT) {
+      DEBUG_PRINTLN(F("DHT timeout waiting for start signal low pulse."));
+      _lastresult = false;
+      return _lastresult;
+    }
+    if (expectPulse(HIGH) == TIMEOUT) {
+      DEBUG_PRINTLN(F("DHT timeout waiting for start signal high pulse."));
+      _lastresult = false;
+      return _lastresult;
+    }
+
+    // Now read the 40 bits sent by the sensor.  Each bit is sent as a 50
+    // microsecond low pulse followed by a variable length high pulse.  If the
+    // high pulse is ~28 microseconds then it's a 0 and if it's ~70 microseconds
+    // then it's a 1.  We measure the cycle count of the initial 50us low pulse
+    // and use that to compare to the cycle count of the high pulse to determine
+    // if the bit is a 0 (high state cycle count < low state cycle count), or a
+    // 1 (high state cycle count > low state cycle count). Note that for speed
+    // all the pulses are read into a array and then examined in a later step.
+    for (int i = 0; i < 80; i += 2) {
+      cycles[i] = expectPulse(LOW);
+      cycles[i + 1] = expectPulse(HIGH);
+    }
+
+    interrupts();
+  } // Timing critical code is now complete.
+
+  // Inspect pulses and determine which ones are 0 (high state cycle count < low
+  // state cycle count), or 1 (high state cycle count > low state cycle count).
+  for (int i = 0; i < 40; ++i) {
+    uint32_t lowCycles = cycles[2 * i];
+    uint32_t highCycles = cycles[2 * i + 1];
+    if ((lowCycles == TIMEOUT) || (highCycles == TIMEOUT)) {
+      DEBUG_PRINTLN(F("DHT timeout waiting for pulse."));
+      _lastresult = false;
+      return _lastresult;
+    }
+    data[i / 8] <<= 1;
+    // Now compare the low and high cycle times to see if the bit is a 0 or 1.
+    if (highCycles > lowCycles) {
+      // High cycles are greater than 50us low cycle count, must be a 1.
+      data[i / 8] |= 1;
+    }
+    // Else high cycles are less than (or equal to, a weird case) the 50us low
+    // cycle count so this must be a zero.  Nothing needs to be changed in the
+    // stored data.
+  }
+
+  DEBUG_PRINTLN(F("Received from DHT:"));
+  DEBUG_PRINT(data[0], HEX);
+  DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[1], HEX);
+  DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[2], HEX);
+  DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[3], HEX);
+  DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[4], HEX);
+  DEBUG_PRINT(F(" =? "));
+  DEBUG_PRINTLN((data[0] + data[1] + data[2] + data[3]) & 0xFF, HEX);
+
+  // Check we read 40 bits and that the checksum matches.
+  if (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
+    _lastresult = true;
+    return _lastresult;
+  } else {
+    DEBUG_PRINTLN(F("DHT checksum failure!"));
+    _lastresult = false;
+    return _lastresult;
+  }
+}
+
+float convertCtoF(float c) { return c * 1.8 + 32; }
+
+float convertFtoC(float f) { return (f - 32) * 0.55555; }
+
+float readTemperature(bool S, bool force) {
+  float f = NAN;
+
+  if (read(force)) {
+    f = data[2];
+    if (data[3] & 0x80) {
+      f = -1 - f;
+    }
+    f += (data[3] & 0x0f) * 0.1;
+    if (S) {
+      f = convertCtoF(f);
+    }
+  }
+  return f;
+}
+
+
+float readHumidity(bool force) {
+  float f = NAN;
+  if (read(force)) {
+      f = data[0] + data[1] * 0.1;
+  }
+  return f;
+}
+
+void begin(uint8_t usec) {
+  // set up the pins!
+  pinMode(_pin, INPUT_PULLUP);
+  // Using this value makes sure that millis() - lastreadtime will be
+  // >= MIN_INTERVAL right away. Note that this assignment wraps around,
+  // but so will the subtraction.
+  _lastreadtime = millis() - MIN_INTERVAL;
+  DEBUG_PRINT("DHT max clock cycles: ");
+  DEBUG_PRINTLN(_maxcycles, DEC);
+  pullTime = usec;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  _bit = digitalPinToBitMask(_pin);
+  _port = digitalPinToPort(_pin);
+  _maxcycles =
+      microsecondsToClockCycles(1000); // 1 millisecond timeout for
+  
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  begin(55);
+}
+
+void loop() {
+  
+  
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = readHumidity(false);
+  // Read temperature as Celsius (the default)
+  float t = readTemperature(false,false);
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = readTemperature(true,false);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.println(f);
+
+}
